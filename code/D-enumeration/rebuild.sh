a9544bf81f839732040d1b0e10628661dac2d269
James D. Mitchell <jdm3@st-andrews.ac.uk>
2020-01-09 11:47:40 +0100

    Adding actual test

--- code/D-enumeration/rebuild.sh
+++ code/D-enumeration/rebuild.sh
@@ -3,4 +3,6 @@ set -e
 
 cp test-d-enum.cpp libsemigroups/tests
 cd libsemigroups && make test_d_enum -j8 
-ln -s libsemigrous/test_d_enum d_enum
+if [ ! -L d_enum ]; then
+  ln -s libsemigroups/test_d_enum d_enum
+fi
