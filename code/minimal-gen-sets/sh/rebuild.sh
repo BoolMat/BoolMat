5e645398ed89b68f8371e6117bff0877add1c02e
James D. Mitchell <jdm3@st-andrews.ac.uk>
2020-01-12 18:48:37 +0100

    Remove superfluous file

--- code/minimal-gen-sets/sh/rebuild.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-cp test-bmat8-enum.cpp libsemigroups/tests
-cd libsemigroups && make test_bmat8_enum -j8 && cd ..
-if [ ! -L test_bmat8_enum ]; then
-  ln -s libsemigroups/test_bmat8_enum test_bmat8_enum
-fi
